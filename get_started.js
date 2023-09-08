0049916cbefb20315b707273333c4e6c0f294f41
Salma3amerr <96180112+Salma3amerr@users.noreply.github.com>
2023-09-08 04:36:03 +0300

    Delete get_started.js

--- get_started.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.querySelector('.button').classList.add('active');
-        document.querySelector('.button2').classList.add('active');
-    }, 3000);
-});
